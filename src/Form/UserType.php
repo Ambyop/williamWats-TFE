2d8ecec60ee240f9f73b9c48403111953dc6694c
William Wats <williamwats98@gmail.com>
2021-05-19 11:04:41 +0200

    creating crud

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo')
+            ->add('lastname')
+            ->add('firstname')
+            ->add('birthday')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('email')
+            ->add('password')
+            ->add('roles')
+            ->add('Ranking')
+            ->add('team')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
