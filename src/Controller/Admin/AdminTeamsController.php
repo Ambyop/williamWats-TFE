d8c13bf3f9d5f0d4a2e8a9f40a3714842a7af260
William Wats <williamwats98@gmail.com>
2021-05-21 16:35:48 +0200

    teams can be created

--- src/Controller/Admin/AdminTeamsController.php
+++ src/Controller/Admin/AdminTeamsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Teams;
+use App\Form\TeamType;
 use App\Repository\TeamsRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +24,31 @@ class AdminTeamsController extends AbstractController
         $teams = $teamsRepository->findAll();
         return $this->render('admin/admin_teams.html.twig', [
             'teams' => $teams,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/teams/create", name="admin_teams_add")
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function addUser(EntityManagerInterface $manager, Request $request): Response
+    {
+        $team = new Teams();
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($team);
+            $manager->flush();
+            $this->addFlash('success', 'L\'Equipe ' . $team->getName() . ' a bien été créé.');
+
+            return $this->redirectToRoute('admin_user');
+        }
+
+        return $this->render("admin/create_team.html.twig", [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/TeamType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Teams;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true
+            ])
+            ->add('division', ChoiceType::class, [
+                'label' => 'Division',
+                'choices' => [
+                    'Nationale 1A' => 'Nationale 1A',
+                    'Nationale 1B' => 'Nationale 1B',
+                    'Nationale 2A' => 'Nationale 2A',
+                    'Nationale 2B' => 'Nationale 2B',
+                    'Nationale 2C' => 'Nationale 2C',
+                    'Régionale A' => 'Régionale A',
+                    'Régionale B' => 'Régionale B',
+                    'Régionale C' => 'Régionale C',
+                    'Provinciale 1A' => 'Provinciale 1A',
+                    'Provinciale 1B' => 'Provinciale 1B',
+                    'Provinciale 2A' => 'Provinciale 2A',
+                    'Provinciale 2B' => 'Provinciale 2B',
+                    'Provinciale 3A' => 'Provinciale 3A',
+                    'Provinciale 3B' => 'Provinciale 3B',
+                ],
+                'required' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Teams::class,
+        ]);
+    }
+}
