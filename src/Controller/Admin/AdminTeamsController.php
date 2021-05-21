b859eb67c718d4331d1c55f30ca9173ab5589d2c
William Wats <williamwats98@gmail.com>
2021-05-21 17:39:38 +0200

    Can update and remove teams

--- src/Controller/Admin/AdminTeamsController.php
+++ src/Controller/Admin/AdminTeamsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Teams;
+use App\Entity\User;
 use App\Form\TeamType;
 use App\Repository\TeamsRepository;
 use App\Repository\UserRepository;
@@ -44,11 +45,49 @@ class AdminTeamsController extends AbstractController
             $manager->flush();
             $this->addFlash('success', 'L\'Equipe ' . $team->getName() . ' a bien été créé.');
 
-            return $this->redirectToRoute('admin_user');
+            return $this->redirectToRoute('admin_teams');
         }
 
         return $this->render("admin/create_team.html.twig", [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/admin/teams/edit/{id}", name="admin_team_edit")
+     * @param Teams $team
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function editTeam(Teams $team, EntityManagerInterface $manager, Request $request)
+    {
+        $form = $this->createForm(TeamType::class, $team);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($team);
+            $manager->flush();
+            $this->addFlash('success', 'Le cours ' . $team->getName() . ' a bien été édité');
+
+            return $this->redirectToRoute('admin_teams');
+        }
+        return $this->render('admin/edit_team.html.twig', [
+            'form' => $form->createView(),
+            'team' => $team
+        ]);
+    }
+
+    /**
+     * @Route("/admin/user/remove/{id}", name="admin_team_remove")
+     * @param Teams $team
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function removeUser(Teams $team, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($team);
+        $manager->flush();
+        $this->addFlash('success', 'L\'équipe ' . $team->getName() . ' a été supprimé');
+        return $this->redirectToRoute('admin_teams');
     }
 }
