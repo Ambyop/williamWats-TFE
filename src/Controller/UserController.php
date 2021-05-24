7f03921bfab3f2f7a09244ef2b6a8a2f57f16c6e
William Wats <williamwats98@gmail.com>
2021-05-24 13:06:54 +0200

    Public and private user profile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserEditType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,10 +15,11 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 class UserController extends AbstractController
 {
     /**
-     * @Route("/profile", name="user_profile")
+     * @Route("/profile", name="user_private_profile")
      * @param UserRepository $userRepository
      * @param TokenStorageInterface $tokenStorage
      * @param Request $request
+     * @param EntityManagerInterface $manager
      * @return Response
      * @throws \Exception
      */
@@ -35,11 +37,31 @@ class UserController extends AbstractController
             $manager->persist($user);
             $manager->flush();
             $this->addFlash('success', 'Votre profil a bien été modifié');
-            return $this->redirectToRoute('user_profile');
+            return $this->redirectToRoute('user_private_profile');
         }
-        return $this->render('user/profile.html.twig', [
+        return $this->render('user/private_profile.html.twig', [
             'user' => $user,
             'editProfileForm' => $formProfile->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_public_profile")
+     * @param User $user
+     * @param UserRepository $userRepository
+     * @param TokenStorageInterface $tokenStorage
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     * @throws \Exception
+     */
+    public function user(User $user,UserRepository $userRepository, TokenStorageInterface $tokenStorage, Request $request, EntityManagerInterface $manager): Response
+    {
+        // load user Data
+        $user = $userRepository->find($user);
+
+        return $this->render('user/public_profile.html.twig', [
+            'user' => $user,
         ]);
     }
 }
