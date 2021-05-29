11ebfcfba91ad2fef80c55fe3c9a8613958e6254
William Wats <williamwats98@gmail.com>
2021-05-29 16:22:26 +0200

    Add Ranking in form when creating manually User

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -60,8 +60,61 @@ class UserType extends AbstractType
                 ],
                 'required' => true
             ])
-            ->add('Ranking',)
-        ;
+            ->add('ranking', ChoiceType::class, [
+                'label' => 'Classement',
+                'choices' => [
+                    'NC' => 'Non-Classé',
+                    'E6' => 'E6',
+                    'E5' => 'E5',
+                    'E4' => 'E4',
+                    'E3' => 'E3',
+                    'E2' => 'E2',
+                    'E1' => 'E1',
+                    'E0' => 'E0',
+                    'D6' => 'D6',
+                    'D5' => 'D5',
+                    'D4' => 'D4',
+                    'D3' => 'D3',
+                    'D2' => 'D2',
+                    'D1' => 'D1',
+                    'D0' => 'D0',
+                    'C6' => 'C6',
+                    'C5' => 'C5',
+                    'C4' => 'C4',
+                    'C3' => 'C3',
+                    'C2' => 'C2',
+                    'C1' => 'C1',
+                    'C0' => 'C0',
+                    'B6' => 'B6',
+                    'B5' => 'B5',
+                    'B4' => 'B4',
+                    'B3' => 'B3',
+                    'B2' => 'B2',
+                    'B1' => 'B1',
+                    'B0' => 'B0',
+                    'A20' => 'A20',
+                    'A19' => 'A19',
+                    'A18' => 'A18',
+                    'A17' => 'A17',
+                    'A16' => 'A16',
+                    'A15' => 'A15',
+                    'A14' => 'A14',
+                    'A13' => 'A13',
+                    'A12' => 'A12',
+                    'A11' => 'A11',
+                    'A10' => 'A10',
+                    'A9' => 'A9',
+                    'A8' => 'A8',
+                    'A7' => 'A7',
+                    'A6' => 'A6',
+                    'A5' => 'A5',
+                    'A4' => 'A4',
+                    'A3' => 'A3',
+                    'A2' => 'A2',
+                    'A1' => 'A1',
+                ],
+                'required' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
