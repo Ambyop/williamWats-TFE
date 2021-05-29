d3cd046a953bcbd77759712beb5b85e1a911056d
William Wats <williamwats98@gmail.com>
2021-05-29 20:55:26 +0200

    Fix contact email

--- src/Service/ContactService.php
+++ src/Service/ContactService.php
@@ -5,23 +5,30 @@ namespace App\Service;
 
 use App\Entity\Contact;
 use Swift_Mailer;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Message;
 use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
 
 class ContactService
 {
-    private Swift_Mailer $mailer;
+    private MailerInterface $mailer;
     private Environment $renderer;
     private ParameterBagInterface $params;
     private array $senderEmails = [];
 
     /**
-     * @param Swift_Mailer $mailer
+     * @param MailerInterface $mailer
      * @param Environment $renderer
      * @param ParameterBagInterface $params
      */
-    public function __construct(Swift_Mailer $mailer, Environment $renderer,ParameterBagInterface $params)
+    public function __construct(MailerInterface $mailer, Environment $renderer,ParameterBagInterface $params)
     {
         $this->params = $params;
         $this->mailer = $mailer;
@@ -33,16 +40,24 @@ class ContactService
         }
     }
 
+    /**
+     * @throws SyntaxError
+     * @throws TransportExceptionInterface
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function sendMail(Contact $contact)
     {
-        $message = (new \Swift_Message())
-            ->setFrom($this->senderEmails['administration'])
-            ->setTo($this->senderEmails['administration'])
-            ->setReplyTo($contact->getEmail())
-            ->setSubject($contact->getSubject())
-            ->setBody($this->renderer->render('contact/email.html.twig', [
+        $message = (new TemplatedEmail())
+            ->from($this->senderEmails['administration'])
+            ->to($this->senderEmails['administration'])
+            ->replyTo($contact->getEmail())
+            ->subject($contact->getSubject())
+            ->htmlTemplate('contact/email.html.twig')
+            ->context([
                 'contact' => $contact
-            ]),'text/html');
+            ])
+            ->embedFromPath('img/laVilette.jpg', 'villette-logo');
         $this->mailer->send($message);
     }
 }
