9e3f0bb0610966d6a746def2b0a65ef9d82c35c6
William Wats <williamwats98@gmail.com>
2021-05-29 19:54:47 +0200

    Fix

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -6,12 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class StaticController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function home(): Response
+    {
+        return $this->render('homepage.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="homepadge")
+     */
+    public function d(): Response
     {
         return $this->render('homepage.html.twig', [
             'controller_name' => 'HomeController',
